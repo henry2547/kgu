8b86dbee64a91fa88d168c51f195d78a89563b45
spectral <spectral@kali>
2025-01-18 08:54:01 +0300

    Initial commit with sensitive information removed

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,12 @@
 <?php  
-$dbcon = mysqli_connect ("localhost", "root", "", "kenya_golf");
+$dbcon = mysqli_connect ("localhost", "USERNAME", "PASSWORD", "kenya_golf");
+
 mysqli_set_charset($dbcon, 'utf8'); 
 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "USERNAME";
+$password = "PASSWORD";
+
 $dbname = "kenya_golf";
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
